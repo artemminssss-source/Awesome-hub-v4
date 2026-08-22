74156e015ebc1e785bfb5cdf850f7719e85596f6
Awesome hub <artemminssss@gmail.com>
2026-08-22 23:40:54 +0300

    Update awesome.lua

--- awesome.lua
+++ awesome.lua
@@ -1,3 +1,4 @@
+—— СПИЗДИЛ КОДЬЭ СЫН УЕБИЩНОЙ ТВАРИ---
 local repo = "https://raw.githubusercontent.com/deividcomsono/Obsidian/main/"
 local Library = loadstring(game:HttpGet(repo .. "Library.lua"))()
 local ThemeManager = loadstring(game:HttpGet(repo .. "addons/ThemeManager.lua"))()
